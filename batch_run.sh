66f3094ebbbca58f0e1f127857b0d9fafa85e773
SparkJiao <jiaofangkai@hotmail.com>
2022-08-17 21:34:59 +0800

    * Add new training script.

--- batch_run.sh
+++ batch_run.sh
@@ -4,7 +4,7 @@ path=$1
 gpu=$2
 
 #for seed in 42 43 44 45 46; do
-#for seed in 42 43; do
-for seed in 44 45 46; do
+for seed in 42 43; do
+#for seed in 44 45 46; do
   source ${path} ${gpu} ${seed}
 done
